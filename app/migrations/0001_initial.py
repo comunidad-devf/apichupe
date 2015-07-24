a8db1e5388554df6cb60609b533a991560f7e9ab
Henoc Díaz <henocdz@gmail.com>
2015-07-24 17:47:41 -0500

    Category Model + Autoslug

---
+++ app/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import autoslug.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+                ('name', models.CharField(max_length=50)),
+                ('slug', autoslug.fields.AutoSlugField(editable=False, populate_from='name', unique=True)),
+            ],
+        ),
+    ]
--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from autoslug import AutoSlugField
 
-# Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=50)
+	slug = AutoSlugField(populate_from='name', unique=True)
+
+	def __str__(self):
+		return "{0}".format(self.name)
