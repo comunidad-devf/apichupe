0923c17c1d8e4723326dd6d46b216e7628e8b07f
Henoc Díaz <henocdz@gmail.com>
2015-07-24 17:53:17 -0500

    Drink Model

---
+++ app/migrations/0002_drink.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Drink',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('name', models.CharField(max_length=100)),
+                ('price', models.FloatField()),
+                ('category', models.ForeignKey(to='app.Category')),
+            ],
+        ),
+    ]
--- app/models.py
+++ app/models.py
@@ -7,3 +7,12 @@ class Category(models.Model):
 
 	def __str__(self):
 		return "{0}".format(self.name)
+
+
+class Drink(models.Model):
+	name = models.CharField(max_length=100)
+	price = models.FloatField()
+	category = models.ForeignKey('app.Category')
+
+	def __str__(self):
+		return "{0} - {1}".format(self.name, self.price)
