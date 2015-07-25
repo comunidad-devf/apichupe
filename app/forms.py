4d74c0c54d2d39ed77247ba6aa36e8f20149616b
Henoc Díaz <henocdz@gmail.com>
2015-07-24 19:12:22 -0500

    Add validation for DrinkResource

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 
-from app.models import Category
+from app.models import Category, Drink
 
 class CategoryForm(forms.ModelForm):
 	class Meta:
 		model = Category
 		fields = ['name']
+
+
+class DrinkForm(forms.ModelForm):
+	class Meta:
+		model = Drink
+		fields = '__all__'
--- app/resources.py
+++ app/resources.py
@@ -3,7 +3,7 @@ from tastypie.authorization import Authorization
 from tastypie.validation import CleanedDataFormValidation
 
 from app.models import Category, Drink
-from app.forms import CategoryForm
+from app.forms import CategoryForm, DrinkForm
 
 class CategoryResource(ModelResource):
 	class Meta:
@@ -16,3 +16,4 @@ class DrinkResource(ModelResource):
 	class Meta:
 		queryset = Drink.objects.all()
 		authorization = Authorization()
+		validation = CleanedDataFormValidation(form_class=DrinkForm)
