be981f57415370942eaa117a27df64f25c4594e9
Henoc Díaz <henocdz@gmail.com>
2015-07-24 19:05:04 -0500

    Add Drink Resources and URL

--- apichupe/urls.py
+++ apichupe/urls.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 
 from tastypie.api import Api
 
-from app.resources import CategoryResource
+from app.resources import CategoryResource, DrinkResource
 
 v1_api = Api('v1')
 
 v1_api.register(CategoryResource())
+v1_api.register(DrinkResource())
 
 
 urlpatterns = [
--- app/resources.py
+++ app/resources.py
@@ -2,7 +2,7 @@ from tastypie.resources import ModelResource
 from tastypie.authorization import Authorization
 from tastypie.validation import CleanedDataFormValidation
 
-from app.models import Category
+from app.models import Category, Drink
 from app.forms import CategoryForm
 
 class CategoryResource(ModelResource):
@@ -10,3 +10,9 @@ class CategoryResource(ModelResource):
 		queryset = Category.objects.all()
 		authorization = Authorization()
 		validation = CleanedDataFormValidation(form_class=CategoryForm)
+
+
+class DrinkResource(ModelResource):
+	class Meta:
+		queryset = Drink.objects.all()
+		authorization = Authorization()
